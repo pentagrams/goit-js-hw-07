2d6554a14c1830e7c5e6b0237bff248ee06fc062
Max <maxminullin@gmail.com>
2021-03-03 12:28:19 +0200

    addtask7end

--- js/task7.js
+++ js/task7.js
@@ -1,1 +1,9 @@
-const inputRef = document.querySelector('#font-size-control')
+const inputRef = document.querySelector('#font-size-control');
+const spanRef = document.querySelector('#text');
+
+inputRef.addEventListener('input', onfontSizeControl);
+
+function onfontSizeControl(event) {
+      let countPixel = 
+      spanRef.style.fontSize = `${Number(event.currentTarget.value) + 50}%`;
+}
